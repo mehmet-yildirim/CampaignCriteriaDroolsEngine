742f44f2cacc313770761ebb92ad36430d780ca0
meyildirim <meyildirim@innova.com.tr>
2017-01-17 17:05:46 +0300

    Initial commit

---
+++ src/main/java/tr/com/innova/campaign/criteria/drools/engine/CampaignCriteriaDroolsEngineApplication.java
@@ -0,0 +1,54 @@
+package tr.com.innova.campaign.criteria.drools.engine;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tr.com.innova.campaign.criteria.drools.engine.amqp.Receiver;
+
+@SpringBootApplication
+public class CampaignCriteriaDroolsEngineApplication {
+
+	final static String queueName = "spring-boot";
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange("spring-boot-exchange");
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange topicExchange) {
+		return BindingBuilder.bind(queue).to(topicExchange).with(queueName);
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver){
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignCriteriaDroolsEngineApplication.class, args);
+	}
+
+
+}
---
+++ src/main/java/tr/com/innova/campaign/criteria/drools/engine/amqp/Receiver.java
@@ -0,0 +1,17 @@
+package tr.com.innova.campaign.criteria.drools.engine.amqp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by meyildirim on 17-Jan-17.
+ */
+@Component
+@Slf4j
+public class Receiver {
+
+    public void receiveMessage(String message) {
+        log.info("Received message: {}", message);
+
+    }
+}
