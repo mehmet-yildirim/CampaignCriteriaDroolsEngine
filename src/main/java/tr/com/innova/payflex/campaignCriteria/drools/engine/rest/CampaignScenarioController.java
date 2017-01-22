19aac3d7e4fdfc72f5594b568b124f6ab2fd37b9
mehmet <meyildirim@innova.com.tr>
2017-01-22 22:38:41 +0300

    docker config

--- src/main/java/tr/com/innova/payflex/campaignCriteria/drools/engine/rest/CampaignScenarioController.java
+++ src/main/java/tr/com/innova/payflex/campaignCriteria/drools/engine/rest/CampaignScenarioController.java
@@ -1,10 +1,7 @@
 package tr.com.innova.payflex.campaignCriteria.drools.engine.rest;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tr.com.innova.payflex.campaignCriteria.drlGenerator.entity.DroolsCampaignMatch;
 import tr.com.innova.payflex.campaignCriteria.drools.engine.service.CampaignCriteriaScenario1Service;
 
@@ -23,7 +20,7 @@ public class CampaignScenarioController {
 
     @RequestMapping(value = "/getCampaign")
     public @ResponseBody
-    DroolsCampaignMatch getAllFacts(@RequestAttribute("segment") String segment) {
+    DroolsCampaignMatch getAllFacts(@RequestParam(value = "segment") String segment) {
 
         return campaignCriteriaScenario1Service.getCampaignEvent(segment);
     }
